c0a38dc387e0ebd32eda76ae607431ac1ca8c66f
Marc Partensky <marc.partensky@gmail.com>
2023-02-16 00:39:54 +0100

    bruteforce

--- web-hydra/__main__.py
+++ web-hydra/__main__.py
@@ -13,5 +13,5 @@ def index():
 
 
 @app.get("/{protocol}/{url}")
-def bruteforce(protocol, str, url: str, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def bruteforce(protocol: str, url: str, q: Union[str, None] = None):
+    return {"protocol": protocol, "url": url, "q": q}
