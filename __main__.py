8853319d1fec9365e9216881e97d3aa1a7fb99ea
Marc Partensky <marc.partensky@gmail.com>
2023-02-16 00:51:34 +0100

    fix

--- __main__.py
+++ __main__.py
@@ -3,5 +3,5 @@
 import uvicorn
 
 uvicorn.run(
-    "web-hydra:app", port=80, host="0.0.0.0", # reload=True, # reload_dirs=["web-hydra"]
+    "web_hydra:app", port=80, host="0.0.0.0", # reload=True, # reload_dirs=["web-hydra"]
 )
