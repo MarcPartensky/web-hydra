2980c13f2cf80e3560673d26b61c025cc59f97b8
Marc Partensky <marc.partensky@gmail.com>
2023-02-16 15:25:22 +0100

    query

--- web_hydra/server.py
+++ web_hydra/server.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 import os
+import subprocess
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from starlette.responses import FileResponse 
 from fastapi.staticfiles import StaticFiles
 
@@ -20,7 +21,19 @@ app.mount("/public", StaticFiles(directory=public_folder), name="public")
 async def index():
     return FileResponse(os.path.join(front_folder, "index.html"))
 
+@app.get("/query")
+def query(q: Union[str, None] = None):
+    return {"q": q}
+
 @app.get("/{protocol}/{url}")
 def bruteforce(protocol: str, url: str, q: Union[str, None] = None):
     return {"protocol": protocol, "url": url, "q": q}
 
+@app.get("/shell")
+def shell(q: Union[str, None] = None):
+    if not q:
+        raise HTTPException(status_code=404, detail="Incomplete get params.")
+    cmd = q["cmd"]
+    words = cmd.split(" ")
+    out = subprocess.run(words, capture_output=True)
+    return {"output": out.stdout}
