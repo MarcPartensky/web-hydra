61035507de389226a830f8ecf4944f5930af6ba1
Marc Partensky <marc.partensky@gmail.com>
2023-02-16 01:34:29 +0100

    serve vue

--- web_hydra/server.py
+++ web_hydra/server.py
@@ -11,14 +11,10 @@ front_folder = "./front/dist"
 
 
 @app.get("/")
-def index():
-    return "Lets get started"
-
+async def index():
+    return FileResponse(os.path.join(front_folder, "index.html"))
 
 @app.get("/{protocol}/{url}")
 def bruteforce(protocol: str, url: str, q: Union[str, None] = None):
     return {"protocol": protocol, "url": url, "q": q}
 
-@app.get("/")
-async def read_index():
-    return FileResponse('index.html')
